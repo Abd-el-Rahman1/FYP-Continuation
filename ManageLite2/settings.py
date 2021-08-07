9bcaf5b2f499324c4a8fc0666e6649c3b4628429
OwlKry <annteeme@gmail.com>
2021-08-08 07:42:25 +0800

    debug complete

--- ManageLite2/settings.py
+++ ManageLite2/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
